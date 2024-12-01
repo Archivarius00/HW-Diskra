adcdbacb05b5b802536cf0c16475dfa8c3982135
Егор Украинец <eukrainets@metaprime.ru>
2024-12-01 13:32:39 +0300

    time error in arrange()

--- main.py
+++ main.py
@@ -2,33 +2,48 @@ import tkinter as tk
 from tkinter import ttk
 import pygame
 from tkinter import *
+import time as t
 
+def arr_check():
+    global check 
+    check = True
 
 def clear():
     entry.delete(0, END)
  
-def display():
-    if cnt == 0:
-            
-        arr = entry.get()
-        print(arr)
-        clear()
-        prog(arr)
+def display():  
+    arr = entry.get()
+    clear()
+    prog(arr)
+
+def arrange():
+    global check
+    while True:
+        t.sleep(0.5)
+        if check == True:
+            break
+    arr = entry.get()
+    check = False
+    clear()
+    return str(arr)
+
 
 def prog(n):
     for k in range(int(n)):
         if k != 0:
             arr = lst
-            list1 = [str(inp) for inp in input().split()]
+            list1 = [str(inp) for inp in arrange()]
             lst = list(set(list1) & set(arr))
         else:
-            list1 = [str(inp) for inp in input().split()]
+            list1 = [str(inp) for inp in arrange()]
             lst = list1
+    
+    ans = ' '.join(lst)
+    print(f"Вот пересечение всех множеств: {{{ans}}}")
 
 
 num = 1
-cnt = 0
-
+check = False
 
 # pygame.init()
 # pygame.mixer.init()
@@ -39,7 +54,7 @@ cnt = 0
 
 window = tk.Tk()
 window.title('Cyberpunk code generator')
-window.geometry('1165x645')
+window.geometry('1165x300') #1165x645
 bg_img = tk.PhotoImage(file='Johny.png')
 
 label_pic = tk.Label(window, image=bg_img)
@@ -51,14 +66,20 @@ frame.place(relx=0.5, rely=0.65, anchor='center')
 
 
 
-entry = ttk.Entry()
-entry.pack(anchor=CENTER, padx=6, pady=6)
+
 
 lbl_result = tk.Label(frame, text = f'Введите элементы множества {num} (через пробелы)', font=('Comic Sans MS', 15))
 lbl_result.grid(column=1, row=1, padx=10, pady=10)
 
+entry = ttk.Entry()
+entry.pack(anchor=CENTER, padx=6, pady=6)
+
 btn_start = tk.Button(frame, text='Done', command=display, font=('Comic Sans MS', 10))
-btn_start.grid(column=1, row=3, padx=10, pady=10)
+btn_start.grid(column=0, row=3, padx=10, pady=10)
+
+btn_start = tk.Button(frame, text='Arr', command=arr_check, font=('Comic Sans MS', 10))
+btn_start.grid(column=2, row=3, padx=10, pady=10)
+
 
 
 window.mainloop()
