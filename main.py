0d9dca16a2323e8045bef9eb657d5c099d0aea02
Егор Украинец <eukrainets@metaprime.ru>
2024-12-03 01:00:54 +0300

    im done

--- main.py
+++ main.py
@@ -1,62 +1,49 @@
 import tkinter as tk
 from tkinter import ttk
-import pygame
 from tkinter import *
 import time as t
 
-def arr_check():
-    next(gen)
+def kill():
+    window.destroy()
 
 def clear():
     entry.delete(0, END)
- 
-def display():  
-    global n
-    n = entry.get()
-    clear()
-    next(gen)
 
-def arrange():
-    arr = entry.get()
-    clear()
-    return arr
+def algos():
+    global arr
+    global lst
+    global k
+    global txt
+    ar = entry.get()
+    if ar.split() == []:
+        txt = 'Вы ничего не ввели!!!'
+    else:
+        clear()
 
-
-def prog():
-    global n
-    print(n)
-    for k in range(int(n)):
         if k != 0:
-            yield
             arr = lst
-            list1 = [str(inp) for inp in arrange().split()]
+            list1 = [str(inp) for inp in ar.split()]
             print(list1)
             lst = list(set(list1) & set(arr))
         else:
-            yield
-            list1 = [str(inp) for inp in arrange().split()]
+            list1 = [str(inp) for inp in ar.split()]
             print(list1)
-            lst = list1
-    
-    ans = ' '.join(lst)
-    print(f"Вот пересечение всех множеств: {{{ans}}}")
+            lst = list1    
+            k += 1
 
+def fin():
+    global lst
+    global nigg
+    nigg = f"Вот пересечение всех множеств: {{{lst}, %}}"
+    print(f"Вот пересечение всех множеств: {lst} и пустое множество ")
 
-num = 1
-check = False
-in_arr = ''
-gen = prog()
-
-pygame.init()
-pygame.mixer.init()
-pygame.mixer.music.load("Cyberpunk 2077 Unreleased OST – The Rebel Path (Cello Version).mp3")
-pygame.mixer.music.play()
-
-
+k = 0
+txt = 'ПРОЧТИТЕ README'
+nigg = '**Пересечение ваших множеств материализуется здесь**'
 
 window = tk.Tk()
 window.title('Cyberpunk code generator')
-window.geometry('1165x300') #1165x645
+window.geometry('1165x645') #1165x645
 bg_img = tk.PhotoImage(file='Johny.png')
 
 label_pic = tk.Label(window, image=bg_img)
@@ -66,21 +53,29 @@ frame.place(relx=0.5, rely=0.65, anchor='center')
 
 
 
-
-
-
-
-lbl_result = tk.Label(frame, text = f'Введите элементы множества {num} (через пробелы)', font=('Comic Sans MS', 15))
+lbl_result = tk.Label(frame, text = txt, font=('Comic Sans MS', 15))
 lbl_result.grid(column=1, row=1, padx=10, pady=10)
 
+
+btn_start = tk.Button(frame, text='Ввести новый массив', command=algos, font=('Comic Sans MS', 10))
+btn_start.grid(column=0, row=2, padx=10, pady=10)
+
+btn_start = tk.Button(frame, text='Я съел все массивы', command=fin, font=('Comic Sans MS', 10))
+btn_start.grid(column=2, row=2, padx=10, pady=10)
+
+btn_start = tk.Button(frame, text='Кончить', command=kill, font=('Comic Sans MS', 10))
+btn_start.grid(column=1, row=4, padx=10, pady=10)
+
+
 entry = ttk.Entry()
-entry.pack(anchor=CENTER, padx=6, pady=6)
+entry.pack(anchor=CENTER, padx=6, pady=50)
 
-btn_start = tk.Button(frame, text='Done', command=display, font=('Comic Sans MS', 10))
-btn_start.grid(column=0, row=3, padx=10, pady=10)
-
-btn_start = tk.Button(frame, text='Arr', command=arr_check, font=('Comic Sans MS', 10))
-btn_start.grid(column=2, row=3, padx=10, pady=10)
-
+lbl_result = tk.Label(frame, text = nigg, font=('Comic Sans MS', 12))
+lbl_result.grid(column=1, row=3, padx=10, pady=10)
 
 window.mainloop()
+
+
+
+
+
---
+++ outdated_code.py
@@ -0,0 +1,83 @@
+import tkinter as tk
+from tkinter import ttk
+import pygame
+from tkinter import *
+import time as t
+
+
+def arr_check():
+    next(gen)
+
+def clear():
+    entry.delete(0, END)
+ 
+def display():  
+    global n
+    n = entry.get()
+    clear()
+    next(gen)
+
+def arrange():
+    arr = entry.get()
+    clear()
+    return arr
+
+def prog():
+    global n
+    print(n)
+    for k in range(int(n)):
+        if k != 0:
+            yield
+            arr = lst
+            list1 = [str(inp) for inp in arrange().split()]
+            print(list1)
+            lst = list(set(list1) & set(arr))
+        else:
+            yield
+            list1 = [str(inp) for inp in arrange().split()]
+            print(list1)
+            lst = list1
+    
+    ans = ' '.join(lst)
+    print(f"Вот пересечение всех множеств: {{{ans}}}")
+
+
+num = 1
+check = False
+in_arr = ''
+gen = prog()
+
+
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load("Cyberpunk 2077 Unreleased OST – The Rebel Path (Cello Version).mp3")
+pygame.mixer.music.play()
+
+
+
+window = tk.Tk()
+window.title('Cyberpunk code generator')
+window.geometry('1165x645') #1165x645
+bg_img = tk.PhotoImage(file='Johny.png')
+
+label_pic = tk.Label(window, image=bg_img)
+label_pic.place(x=0, y=0, relwidth=1, relheight=1)
+frame = tk.Frame(window)
+frame.place(relx=0.5, rely=0.65, anchor='center')
+
+
+
+lbl_result = tk.Label(frame, text = f'Введите элементы множества {num} (через пробелы)', font=('Comic Sans MS', 15))
+lbl_result.grid(column=1, row=1, padx=10, pady=10)
+
+entry = ttk.Entry()
+entry.pack(anchor=CENTER, padx=6, pady=6)
+
+btn_start = tk.Button(frame, text='Done', command=display, font=('Comic Sans MS', 10))
+btn_start.grid(column=0, row=3, padx=10, pady=10)
+
+btn_start = tk.Button(frame, text='Arr', command=arr_check, font=('Comic Sans MS', 10))
+btn_start.grid(column=2, row=3, padx=10, pady=10)
+
+
+window.mainloop()
